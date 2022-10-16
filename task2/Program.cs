1bee8901e9133f1c0c3ec93766b86d2a75a886fa
Ildar <steel14@yandex.ru>
2022-10-16 21:28:04 +0500

    added task2

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumBetween(int M, int N)
+{
+    if (N == M) return M;
+    return N + SumBetween(M, N-1);
+}
+
+System.Console.WriteLine(SumBetween(1,15));
